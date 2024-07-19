86e070dc9e62effbd30497fb2f7cf688592fc14d
Сергей <sergey.vasilev.98@mail.ru>
2024-07-19 23:47:27 +0300

    Создаем класс для записи данных в json

--- src/api.py
+++ src/api.py
@@ -1,3 +1,4 @@
+import json
 from abc import ABC, abstractmethod
 
 
@@ -7,3 +8,15 @@ class BaseHH(ABC):
     def __init__(self):
         pass
 
+class Parser(BaseHH):
+
+    def __init__(self, file_worker) -> None:
+        self.file_worker = file_worker
+
+    def save_in_file(self, vacancies) -> None:
+        """
+        Метод класса Parser(BaseHH) записывающий в файл *.json всю информацию по вакансиям
+        :param vacancies: список вакансий полученных с сайта hh.ru по запросу
+        """
+        with open(self.file_worker, "w", encoding="utf-8") as file:
+            json.dump(vacancies, file, indent=4)
