e173ce3eda83eab2ca71140d1e01c39ff6c2cead
Сергей <sergey.vasilev.98@mail.ru>
2024-07-19 23:53:36 +0300

    Дописываем и создаем статик метод ля валидации

--- src/vacancy.py
+++ src/vacancy.py
@@ -10,3 +10,26 @@ class WorkVacancy:
         self.url_vacancy = url_vacancy
         self.city = city
 
+        if self._validate_salary(salary_from) is None:
+            if salary_from is None:
+                self.salary_from = 0
+            else:
+                self.salary_from = salary_from
+
+        if self._validate_salary(salary_to) is None:
+            if salary_to is None:
+                self.salary_to = 0
+            else:
+                self.salary_to = salary_to
+
+        self.salary_currency = salary_currency
+        self.snippet_requirement = snippet_requirement
+
+    @staticmethod
+    def _validate_salary(salary):
+        """
+        Метод класса осуществляющий валидацию заработной платы вакансий
+        :param salary: зарплата передававемая при инициализации, либо salary_to, либо salary_from
+        """
+        if salary is not None and salary < 0:
+            raise ValueError("Salary cannot be negative")
