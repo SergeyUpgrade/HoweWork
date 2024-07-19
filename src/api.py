da620ece096ea978815258866841b7bf8cd1b15b
Сергей <sergey.vasilev.98@mail.ru>
2024-07-19 23:50:40 +0300

    Создаем метод класса который запрашивает информацию

--- src/api.py
+++ src/api.py
@@ -1,6 +1,8 @@
 import json
 from abc import ABC, abstractmethod
 
+import requests
+
 
 class BaseHH(ABC):
     """Класс-конструктор для работы с API HH"""
@@ -30,3 +32,16 @@ class HH(Parser):
         self.params = {'text': '', 'page': 0, 'per_page': 100}
         self.vacancies = []
         super().__init__(file_worker)
+
+    def load_vacancies(self, keyword) -> None:
+        """
+        Метод класса HH(Parser), который запрашивает информацию по вакаснсиям на сайте hh.ru
+        :param keyword: наименовании запрашиваемой вакансии
+        """
+        self.params['text'] = keyword
+        while self.params.get('page') != 20:
+            response = requests.get(self.url, headers=self.headers, params=self.params, timeout=10)
+            vacancies = response.json()['items']
+            self.vacancies.extend(vacancies)
+            self.params['page'] += 1
+        super().save_in_file(self.vacancies)
